10080f2d960d7569daa95a02064ee3e3c645c7e2
NicolasLfs <nicolaslopes03@hotmail.com>
2024-06-10 17:56:42 -0300

    Att_pages_company_contact

--- src/components/pages/Company.js
+++ src/components/pages/Company.js
@@ -1,5 +1,67 @@
+import styles from "./Home.module.css";
+
 function Company() {
-    return <h1>Company</h1>
+  return (
+    <section className={styles.home_container}>
+      <h1>Company</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
+        dignissim tempor mi vestibulum aliquet. Nullam a cursus eros. Integer
+        mattis mauris consectetur scelerisque porttitor. Pellentesque accumsan
+        accumsan urna eleifend fermentum. Fusce non diam ut nibh elementum
+        porttitor pulvinar et est. Sed pretium ipsum vulputate, blandit tellus
+        sit amet, elementum diam. Aliquam erat volutpat. In bibendum nibh metus,
+        non mollis nisl consectetur ac. Sed sodales elementum molestie. Nam quis
+        odio nisi. Aliquam semper, tortor non porta ultrices, enim magna tempor
+        enim, a luctus tellus mauris sollicitudin ligula. Aliquam pharetra sit
+        amet erat at tempus. Orci varius natoque penatibus et magnis dis
+        parturient montes, nascetur ridiculus mus. Integer posuere augue in dui
+        cursus, et mollis urna maximus. Maecenas in blandit lacus, ut lobortis
+        eros. Nulla porttitor risus non cursus condimentum. Aliquam faucibus,
+        diam ullamcorper gravida euismod, magna nunc suscipit velit, imperdiet
+        bibendum ante magna eu urna. Integer luctus quam rutrum interdum
+        suscipit. Maecenas posuere risus sed libero fringilla, vel dictum eros
+        cursus. Mauris nisl ipsum, ornare eu volutpat ac, efficitur efficitur
+        mi. Nulla ultrices quam vitae egestas facilisis. Suspendisse potenti.
+        Aliquam erat volutpat. Fusce volutpat iaculis nibh eu imperdiet.
+        Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere
+        cubilia curae; Morbi varius magna quis ante suscipit, ut congue quam
+        suscipit. Donec ipsum mi, pellentesque accumsan augue id, fringilla
+        aliquam sem. Praesent ullamcorper libero eget laoreet mollis. Nulla vel
+        elit felis. Fusce non elit nec diam vehicula porttitor maximus sed
+        massa. Morbi consequat quam at libero volutpat volutpat. Donec lobortis
+        nec erat a rhoncus. Etiam in dapibus purus, vel tempor mi. Mauris non
+        feugiat eros. Aenean efficitur tellus ut diam suscipit, laoreet
+        pellentesque mauris laoreet. Vivamus aliquam nulla velit, non dictum
+        diam convallis in. Etiam ultricies ultricies eros, et dapibus metus
+        feugiat et. Nunc feugiat faucibus odio interdum interdum. Vestibulum sit
+        amet libero eros. Nullam nec sapien fringilla, laoreet quam rhoncus,
+        venenatis nunc. Ut vitae quam accumsan, volutpat nisl at, viverra nibh.
+        Ut sed magna nulla. Nam eget metus tellus. Praesent sed arcu ut erat
+        finibus viverra. Aliquam quis mi in urna interdum dignissim id eget
+        odio. Aliquam placerat lectus id urna ultrices, non porta nunc varius.
+        Phasellus vulputate malesuada ligula vitae finibus. Maecenas tempor
+        justo a ipsum efficitur, quis elementum sapien malesuada. Sed vel
+        vulputate nisi, non consectetur ligula. Nunc aliquet nibh vel volutpat
+        bibendum. Vivamus porta elit et vulputate euismod. Vivamus id rhoncus
+        sapien. Nullam at lacus lacus. Sed ornare consectetur eleifend. Maecenas
+        tempor turpis sit amet ex rutrum rhoncus. Aliquam nec nisi imperdiet
+        urna placerat finibus. Suspendisse vitae iaculis lacus. In eleifend,
+        tortor ac ultrices aliquam, libero augue scelerisque est, at sodales
+        orci libero vel justo. Nam ultricies euismod magna et cursus. Quisque
+        volutpat, magna eu viverra sodales, nibh mauris euismod felis, id
+        laoreet arcu nisi ut arcu. Donec a luctus tortor, quis eleifend tortor.
+        Phasellus non imperdiet elit, eget posuere eros. Mauris vel sem quis
+        mauris mollis blandit. Nulla lobortis diam a massa viverra, sed commodo
+        nunc commodo. Maecenas sit amet justo interdum, egestas odio a, viverra
+        turpis. Curabitur fermentum, orci in pharetra porta, mi metus auctor
+        ante, id cursus turpis orci non justo. In vulputate tortor mollis dolor
+        feugiat, in viverra quam condimentum. Integer ut mollis felis. Quisque
+        eu urna augue. Sed egestas, lectus ut iaculis bibendum, enim tellus
+        lobortis lorem, in lacinia velit tortor sed nunc.
+      </p>
+    </section>
+  );
 }
 
-export default Company
+export default Company;
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,5 +1,11 @@
+import styles from "./Home.module.css";
+
 function Contact() {
-    return <h1>Contact</h1>
+  return (
+    <section className={styles.home_container}>
+      <h1>Contact</h1>
+    </section>
+  );
 }
 
-export default Contact
+export default Contact;
